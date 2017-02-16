3ac9999fd3119b72fcccebdc53b366fbba368bdb
tretom2121 <t.trencsenyi@gmail.com>
2017-02-16 16:58:44 +0100

    end of day

---
+++ SomeExample/SomeExample/Model/XmlInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeExample.Model
+{
+    public class XmlInput : IInputFile
+    {
+        public string ReadContext(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SomeExample/SomeExample/ViewModel/MainViewModel.cs
+++ SomeExample/SomeExample/ViewModel/MainViewModel.cs
@@ -1,13 +1,76 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using SomeExample.Model;
+using System;
+using System.Diagnostics;
+using System.Windows;
 
 namespace SomeExample.ViewModel
 {
  
     public class MainViewModel : ViewModelBase
     {
-          public MainViewModel()
+        private bool _txtChk;
+        public bool txtChk
         {
-      
+            get { return _txtChk; }
+            set { Set (ref _txtChk, value); }
+        }
+
+        private bool _xmlChk;
+        public bool xmlChk
+        {
+            get { return _xmlChk; }
+            set { Set(ref _xmlChk, value); }
+        }
+
+        private bool _jsonChk;
+        public bool jsonChk
+        {
+            get { return _jsonChk; }
+            set { Set(ref _jsonChk, value); }
+        }
+
+        private bool _canExitChecker;
+        public bool canExitChecker
+        {
+            get { return _canExitChecker; }
+            set
+            {
+                Set(ref _canExitChecker, value);
+                ExitCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private string _fileContent;
+        public string fileContent
+        {
+            get { return _fileContent; }
+            set { Set(ref _fileContent, value); }
+        }
+
+        public RelayCommand LoadFileCommand { get; private set; }
+        public RelayCommand ExitCommand { get; private set; }
+
+#region ctor
+        public MainViewModel()
+        {
+            LoadFileCommand = new RelayCommand(LoadFile);
+            ExitCommand = new RelayCommand(Exit, ()=> { return canExitChecker; });
+        }
+#endregion //ctor
+
+        private void LoadFile()
+        {
+            if (txtChk)
+            {
+            }
+            Debug.WriteLine("LoadFileCommand called");
+        }
+
+        private void Exit()
+        {
+            Application.Current.Shutdown();
         }
     }
 }
